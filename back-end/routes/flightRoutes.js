fa7934a88a92cde77d5329d0b6e69d9eb7ca450c
Badr AlSayed <badrmohamed1000@gmail.com>
2021-10-31 20:16:07 +0200

    delete route for flights

--- back-end/routes/flightRoutes.js
+++ back-end/routes/flightRoutes.js
@@ -23,6 +23,15 @@ router
     res.send(newFlight);
   })
   .put(async (req, res) => {})
-  .delete(async (req, res) => {});
+ 
+ router.delete('/:id', async (req,res) =>{
+     const {id} = req.params;
+    
+     if(!mongoose.Types.ObjectId.isValid(id))
+          return res.send(`No flight with ${id}`)
+    
+        await Flight.findByIdAndDelete(id);
+        res.json('Deleted');
+      });
 
 module.exports = router;
