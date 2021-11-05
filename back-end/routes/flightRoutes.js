04e162aab580b7a804b110e40794dd5b0ea638e8
Badr AlSayed <badrmohamed1000@gmail.com>
2021-11-05 19:39:06 +0200

    Fixed bug with the Delete Pop up

--- back-end/routes/flightRoutes.js
+++ back-end/routes/flightRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Flight = require("../models/flight");
-const mongoose =  require("mongoose");
+const mongoose = require("mongoose");
 
 router
   .route("/")
@@ -11,7 +11,7 @@ router
   })
   .post(async (req, res) => {
     const flight = new Flight({
-      flightNumber: 155,
+      flightNumber: 101,
       departure: 24 / 12 / 2020,
       arrival: 25 / 12 / 2020,
       economySeats: 213,
@@ -21,16 +21,16 @@ router
     const newFlight = await flight.save();
     res.send(newFlight);
   })
-  .put(async (req, res) => {})
- 
- router.delete('/:id', async (req,res) =>{
-     const {id} = req.params;
-    
-     if(!mongoose.Types.ObjectId.isValid(id))
-          return res.send(`No flight with ${id}`)
-    
-        await Flight.findByIdAndDelete(id);
-        res.json('Deleted');
-      });
+  .put(async (req, res) => {});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.send(`No flight with ${id}`);
+
+  await Flight.findByIdAndDelete(id);
+  res.json("Deleted");
+});
 
 module.exports = router;
---
+++ front-end/src/components/Popups/DeletePopUp.js
@@ -0,0 +1,69 @@
+import axios from "axios";
+import Modal from "@mui/material/Modal";
+import { useState } from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+const Popup = ({ flight, setOriginalFlights, popupText }) => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const fetchFlights = async () => {
+    const result = await axios("http://localhost:5000/flight");
+    setOriginalFlights(result.data);
+  };
+
+  const onDelete = async (id) => {
+    await axios.delete(`http://localhost:5000/flight/${id}`, { _id: id });
+    fetchFlights();
+  };
+
+  return (
+    <div>
+      <Button size="small" onClick={handleOpen}>
+        Delete
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: 400,
+            bgcolor: "background.paper",
+            border: "2px solid #000",
+            boxShadow: 24,
+            p: 4,
+          }}
+        >
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            {popupText}
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            <Button
+              size="small"
+              onClick={() => {
+                onDelete(flight._id);
+                handleClose();
+              }}
+            >
+              Delete
+            </Button>
+            <Button size="small" onClick={handleClose}>
+              Go back
+            </Button>
+          </Typography>
+        </Box>
+      </Modal>
+    </div>
+  );
+};
+
+export default Popup;
